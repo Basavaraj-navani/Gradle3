f6ee03ae11a62f7d94253ba134acd68f5dd9908c
Basavaraj-navani <basavarajnavani77@gmail.com>
2026-03-05 10:49:27 +0530

    Initial commit

---
+++ src/test/java/org/example/WebpageTest.java
@@ -0,0 +1,39 @@
+package org.example;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertTrue;
+
+public class WebpageTest {
+    private static WebDriver driver;
+
+    @BeforeTest
+    public void openBrowser() throws InterruptedException {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        Thread.sleep(2000);
+        driver.get("https://github.com/Basavaraj-navani/Gradle3");
+    }
+
+    @Test
+    public void titleValidationTest(){
+        String actualTitle = driver.getTitle();
+        String expectedTitle = "Tripillar Solutions";
+        Assert.assertEquals(actualTitle, expectedTitle);
+        assertTrue(true, "Title should contain 'Tripillar'");
+
+    }
+
+    @AfterTest
+    public void closeBrowser() throws InterruptedException {
+        Thread.sleep(10000);
+        driver.quit();
+    }
+
+
+}
+
